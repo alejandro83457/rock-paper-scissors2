6e9271655f0c6f11591b68d7679a29c5872796db
alejandro83457 <martinezalejandro2001@hotmail.com>
2026-03-14 20:12:29 -0700

    Add random PC choice

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('test');
+function getComputerChoice() {
+  // Returns the floor of a random int (0-2)+1
+  return Math.floor(Math.random() * 3) + 1;
+}
+
+console.log(getComputerChoice());
